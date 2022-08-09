a32c5decefdf0809a582377fe8ca0e9aa907932b
Samuel <kalusam619@gmail.com>
2022-08-09 08:10:50 +0100

    local staged changes

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,16 +1,18 @@
-def bubblesort(list):
+def bubble_sort(array):
+   '''
+      Sorts an array by bubbling each adjacent elements of the array, and then pushing the smaller one to the left
 
-# Swap the elements to arrange in order
-   for iter_num in range(len(list)-1,0,-1):
-      for idx in range(iter_num):
-         if list[idx]>list[idx+1]:
-            temp = list[idx]
-            list[idx] = list[idx+1]
-            list[idx+1] = temp
+   '''
+   size = len(array)
+   for i in range(1,size):
+      for j in range(size-1):
+         if array[j] > array[j+1]:
+            # Swaps the elements 🙂
+            array[j], array[j+1] = array[j+1] ,array[j]
 
 
 
-my_list = [19,2,31,45,6,11,121,27]
-bubblesort(my_list)
-print(my_list)
+list = [200, 1, 32, 4, -100, 20, 50, 3, 234, 2343, 238234, 2, 5,3, 2,4]
+bubble_sort(list)
+print(list)
 
